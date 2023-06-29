7d1ee5e228541b1b7c42c60a55d2606765ef0e2b
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-06-29 15:02:01 +0530

    sadsd

--- src/view/data/data.js
+++ src/view/data/data.js
@@ -3,6 +3,6 @@ export const menu = ["Home","Login","Register",]
 
 export const product=[
     {id:1,productname:"ADSL Splitter",rate:55,image:require('../images/ADSL Splitter/0023006_adsl-splitter.jpeg'),allimage:[require('../images/ADSL Splitter/0023006_adsl-splitter.jpeg'),require('../images/ADSL Splitter/0023007_adsl-splitter.jpeg'),require('../images/ADSL Splitter/0023008_adsl-splitter.jpeg'),require('../images/ADSL Splitter/0023009_adsl-splitter.jpeg'),require('../images/ADSL Splitter/0023010_adsl-splitter.jpeg')]},
-    {id:2,productname:"Dlink Dual Face Plate",rate:66,image:require('../images/Dlink Dual Face Plate/0016988_dlink-dual-face-plate.jpeg'),allimage:[require('../images/Dlink Dual Face Plate/0016988_dlink-dual-face-plate.jpeg'),require('../images/'),require('../images/'),require('../images/'),require('../images/')]},
+    {id:2,productname:"Dlink Dual Face Plate",rate:66,image:require('../images/Dlink Dual Face Plate/0016988_dlink-dual-face-plate.jpeg'),allimage:[require('../images/Dlink Dual Face Plate/0016988_dlink-dual-face-plate.jpeg')]},
     
 ]
--- src/view/screen/HomeScreen.js
+++ src/view/screen/HomeScreen.js
@@ -1,16 +1,23 @@
 import { useState } from "react"
-
+import {product} from '../data/data'
 export default function HomeScreen(){
   const [user,setUser] = useState(localStorage.getItem("user"))
+const[product1,setProduct] =useState(product)
 
   function logout(){
     localStorage.clear()
     window.location.reload()
   }
+
+  console.log(product1);
     return(
       <>
       
       <h1> HomeScreen</h1>
+      {product1.map(d=>(
+        <img src={d.image}  className="prodcutimage"/>
+      ))
+}
      </>
     )
   }
